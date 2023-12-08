7555b1f499d726cc38faaabe27452705cd67fe8c
Saviour-Dagadu <savicane@gmail.com>
2023-12-07 20:25:11 -0800

    lists

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -3,20 +3,25 @@
 #include <Python.h>
 
 /**
- * print_python_list_info - Prints basic info about Python lists
- * @p: Pointer to a Python object (assumed to be a list)
+ * print_python_list_info - Prints basic info about Python lists.
+ * @p: A PyObject list.
  */
-void print_python_list_info(PyObject *p) {
-    Py_ssize_t size, i;
-    PyObject *item;
+void print_python_list_info(PyObject *p)
+{
+	int size, alloc, i;
+	PyObject *obj;
 
-    size = PyList_Size(p);
+	size = Py_SIZE(p);
+	alloc = ((PyListObject *)p)->allocated;
 
-    printf("[*] Size of the Python List = %ld\n", size);
-    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", alloc);
 
-    for (i = 0; i < size; i++) {
-        item = PyList_GetItem(p, i);
-        printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
-    }
+	for (i = 0; i < size; i++)
+	{
+		printf("Element %d: ", i);
+
+		obj = PyList_GetItem(p, i);
+		printf("%s\n", Py_TYPE(obj)->tp_name);
+	}
 }
--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,55 +3,31 @@
 #include <stdio.h>
 
 /**
-*add_nodeint - adds a new node at the beginning of a listint_t list
-*@head: head of listint_t
-*@n: int to add in listint_t list
-*Return: address of the new element, or NULL if it failed
-*/
-listint_t *add_nodeint(listint_t **head, const int n)
+ * palindrom - recursive alind or not
+ * @head: head list
+ * Return: 0 if it is not a palindrome
+ * 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
 {
-	listint_t *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = *head;
-	*head = new;
-	return (new);
+	if (head == NULL || *head ++ NULL)
+		return (1);
+	return (aux_palind(head, *head));
 }
+
 /**
-*is_palindrome - identify if a syngle linked list is palindrome
-*@head: head of listint_t
-*Return: 1 if it is palindrome else 0
-*/
-int is_palindrome(listint_t **head) {
-    listint_t *slow = *head, *fast = *head;
-    listint_t *prev = NULL, *temp, *mid = NULL;
-    int palindrome = 1;
-
-    if (*head == NULL || (*head)->next == NULL)
-        return 1;
-
-    while (fast != NULL && fast->next != NULL) {
-        fast = fast->next->next;
-
-        temp = slow->next;
-        slow->next = prev;
-        prev = slow;
-        slow = temp;
-    }
-
-    mid = (fast == NULL) ? slow : slow->next;
-
-    while (mid != NULL) {
-        if (mid->n != prev->n) {
-            palindrome = 0;
-            break;
-        }
-        mid = mid->next;
-        prev = prev->next;
-    }
-
-    return palindrome;
+ * aux_palind - funt to know if is palindrome
+ * @head: head list
+ * @end: end list
+ */
+int aux_palind(listint_t *head, listin_t *end)
+{
+	if (end == NULL)
+		return (1);
+	if (aux_palind(head, end->next) && (*head)->n == end->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	return (0);
 }
