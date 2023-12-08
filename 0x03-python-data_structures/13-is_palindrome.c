e6b1074d030867921da21aeffbf1f8979a07765a
Saviour-Dagadu <savicane@gmail.com>
2023-12-07 19:39:06 -0800

    Addding 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -25,29 +25,33 @@ listint_t *add_nodeint(listint_t **head, const int n)
 *@head: head of listint_t
 *Return: 1 if it is palindrome else 0
 */
-int is_palindrome(listint_t **head)
-{
-	listint_t *head2 = *head;
-	listint_t *aux = NULL, *aux2 = NULL;
+int is_palindrome(listint_t **head) {
+    listint_t *slow = *head, *fast = *head;
+    listint_t *prev = NULL, *temp, *mid = NULL;
+    int palindrome = 1;
 
-	if (*head == NULL || head2->next == NULL)
-		return (1);
-	while (head2 != NULL)
-	{
-		add_nodeint(&aux, head2->n);
-		head2 = head2->next;
-	}
-	aux2 = aux;
-	while (*head != NULL)
-	{
-		if ((*head)->n != aux2->n)
-		{
-			free_listint(aux);
-			return (0);
-		}
-		*head = (*head)->next;
-		aux2 = aux2->next;
-	}
-	free_listint(aux);
-	return (1);
+    if (*head == NULL || (*head)->next == NULL)
+        return 1;
+
+    while (fast != NULL && fast->next != NULL) {
+        fast = fast->next->next;
+
+        temp = slow->next;
+        slow->next = prev;
+        prev = slow;
+        slow = temp;
+    }
+
+    mid = (fast == NULL) ? slow : slow->next;
+
+    while (mid != NULL) {
+        if (mid->n != prev->n) {
+            palindrome = 0;
+            break;
+        }
+        mid = mid->next;
+        prev = prev->next;
+    }
+
+    return palindrome;
 }
